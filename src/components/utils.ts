00d7284569c222265938ae56246f48e60e64b8dd
Balamurugan P <b.palanisamy.perficient.com>
2021-08-24 10:03:33 +0530

    notification component added

---
+++ src/components/utils.ts
@@ -0,0 +1,12 @@
+// export const getNotificationPosition = (currentX: number, width: number): number => {
+//     let viewportWidth = window.innerWidth;
+//     debugger
+//     if (viewportWidth < currentX + width) {
+//         debugger
+//         return viewportWidth - (currentX + width);
+//     } else {
+
+//     }
+
+//     return 40;
+// }
